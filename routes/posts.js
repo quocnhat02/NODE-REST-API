be9a18e744d4fbfd5b5717f1ac095a589f49e26a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-20 19:22:04 +0700

    update post

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,20 @@ router.post("/", async (req, res) => {
 });
 
 // update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You can update only your post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // delete a post
 // like a post
 // get a post
