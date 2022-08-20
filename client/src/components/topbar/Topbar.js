c73ee7d70b3713117437f185b31f13edd0eca78a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-20 23:29:29 +0700

    topbar component 1

--- client/src/components/topbar/Topbar.js
+++
@@ -1,7 +0,0 @@
-import "topbar.css";
-
-const Topbar = () => {
-  return <div>Topbar</div>;
-};
-
-export default Topbar;
---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,44 @@
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
+
+const Topbar = () => {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search />
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="Search for friend, post or video"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">HomePage</span>
+          <span className="topbarLink">TimeLine</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
