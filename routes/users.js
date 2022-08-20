4e4858c01151b51abbda362d55e677f92e39d01f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-20 15:44:09 +0700

    delete user

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // update user
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -28,7 +28,21 @@ router.put("/:id", async (req, res) => {
     return res.status(403).json("You can update only your account");
   }
 });
+
 // delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account");
+  }
+});
+
 // get a user
 // follow a user
 // unFollow a user
