44c1a5133fd60ed33b8e129926a82a0b6a9b3143
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-20 12:07:09 +0700

    middleware and first request

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 const app = express();
+const PORT = process.env.PORT || 6969;
 
+// connection MongoDB
 mongoose
   .connect(process.env.MONGO_URL, { serverSelectionTimeoutMS: 5000 })
   .then(() => console.log(`Connected to MONGODB`))
   .catch((error) => console.log(`Fail connect to MONGODB, ${error.message}`));
 
-app.listen(8800, () => {
-  console.log(`Backend server is running!`);
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Homepage");
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port: ${PORT}`);
 });
