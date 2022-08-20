dcf9d28d66eb15ca6ed76ead519edabed8eaf9c3
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-20 15:30:41 +0700

    update user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("It is user routes");
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
 });
+// delete user
+// get a user
+// follow a user
+// unFollow a user
 
 export default router;
